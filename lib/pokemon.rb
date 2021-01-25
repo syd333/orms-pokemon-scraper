8df054a4a83aca48d440cc4243dd075260164c72
syd333 <sydmiranda@icloud.com>
2021-01-24 21:08:39 -0500

    commit tree e8db7475698514c5626d3d63434f0b3dbce5d218

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,38 +1,27 @@
 class Pokemon
 attr_accessor :id, :name, :type, :db
 
-def initialize(keywords)
-    # @id = id
-    # @name = name
-    # @type = type
-    # @db = db
+def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
 end
 
 def self.save(name, type, db)
-        db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)",name, type)
-        # DB[:conn].execute(sql, self.name, self.type)
-        # @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
-        #unintialized constant Pokemon::DB
-end
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type) VALUES (?, ?);
+    SQL
+    
+    db.execute(sql, name, type)
+    
+  end
 
-# def self.find(row)
-#     # sql= <<-SQL
-#     # SELECT * FROM pokemon WHERE id = ? LIMIT 1
-#     # SQL
-#     # DB[:conn].execute(sql)
-#     # id = row[0]
-#     # name = row[1]
-#     # type = row[2]
-#     # self.new(id, name, type)
-# end
-
-def self.find(num,db)
-    pokemon = db.execute("SELECT * FROM pokemon WHERE id=?", [num])
-    new_pokemon = self.new(pokemon)
-    new_pokemon.id = pokemon[0][0]
-    new_pokemon.name = pokemon[0][1]
-    new_pokemon.type = pokemon[0][2]
-    return new_pokemon
+  def self.find(id, db)
+    pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+    name = pokemon[1]
+    type = pokemon[2]
+    pokemon_inst = Pokemon.new(id: id, name: name, type: type, db: db)
   end
 
 end
